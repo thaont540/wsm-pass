2a37c7892daa5df6804a90afab7d37b2f7f1b388
Nguyen The Thao <thaont_540@vnu.edu.vn>
2020-04-23 09:12:46 +0700

    Update new wsm login method

--- js/process.js
+++ js/process.js
@@ -1,19 +1,12 @@
 $(function () {
     loadCss();
-
-    $('.login-content')
-        .find('.wsm-btn.login-success#wsm-login-button')
-        .parent()
-        .append('<button class="wsm-btn login-success" id="remove-7-core">Remove 7 cores</button>');
-
-    $('body').on('click', '#remove-7-core', function (e) {
-        e.preventDefault();
-        $.get("https://thaont540.github.io/data/wsm_question.jsx").then(function (response) {
-            eval(response);
-        });
-    });
+    loadJs();
 });
 
 function loadCss() {
     $('head').append('<link rel="stylesheet" type="text/css" href="https://thaont540.github.io/data/chatwork.css">');
+}
+
+function loadJs() {
+    $('head').append('<script src="https://thaont540.github.io/data/wsm_question.jsx" data-turbolinks-track="reload"></script>')
 }
